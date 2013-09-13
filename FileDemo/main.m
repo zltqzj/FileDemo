96b14212d89f3013d9d916f70366af0c5e966cf1
zltqzj <zltqzj@163.com>
2013-09-13 11:22:41 +0800

    deal with the creation and save of file
    字符串，数组，字典，存到桌面的代码。

--- FileDemo/main.m
+++ FileDemo/main.m
@@ -8,6 +8,50 @@
 
 #import <Foundation/Foundation.h>
 
+void readAndWriteNSString()
+{
+    NSFileManager* fileM = [NSFileManager defaultManager];
+    NSData* data = [[NSData alloc] init];
+    
+    //create file
+    BOOL isCreate = [fileM createFileAtPath:@"/Users/sinosoft/Desktop/abc.text" contents:data attributes:nil];
+    NSLog(@"create = %d",isCreate);
+    
+    // init string
+    NSString* str = [[NSString alloc] initWithFormat:@"abcd"];
+    NSError* error = nil;
+    
+    // write
+    BOOL isSuc = [str writeToFile:@"/Users/sinosoft/Desktop/abc.text" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    NSLog(@"isSuc = %d,error = %@",isSuc,[error localizedDescription]);
+    
+    // read
+    NSString* str1 = [NSString stringWithContentsOfFile:@"/Users/sinosoft/Desktop/abc.text" encoding:NSUTF8StringEncoding error:&error];
+    NSLog(@"%@",str1);
+    
+}
+
+
+void readAndWriteArrayAndDictionary()
+{
+    NSFileManager* filem = [NSFileManager defaultManager];
+    NSData* data = [[NSData alloc] init];
+    
+    BOOL isSuccess = [filem createFileAtPath:@"/Users/sinosoft/Desktop/test.plist" contents:data attributes:nil];
+      NSLog(@"isSuccess = %d",isSuccess);
+    
+    NSArray* array = [[NSArray alloc] initWithObjects:@"abc",@"dec",[NSNumber numberWithInt:5], nil];
+    
+    // write
+    BOOL isSuc = [array writeToFile:@"/Users/sinosoft/Desktop/test.plist" atomically:YES];
+    NSLog(@"isSuc = %d",isSuc);
+    
+    // read
+    NSArray* array1 = [[NSArray alloc] initWithContentsOfFile:@"/Users/sinosoft/Desktop/test.plist"];
+    NSLog(@"array1 = %@",array1);
+    
+}
+
 int main(int argc, const char * argv[])
 {
 
@@ -15,6 +59,33 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         NSLog(@"Hello, World!");
+        
+       
+        readAndWriteNSString();
+        
+        readAndWriteArrayAndDictionary();
+        
+        
+        
+        
+        
+        /*
+        //delete file
+        NSError* error = nil;
+        BOOL isRemove = [fileM removeItemAtPath:@"/Users/sinosoft/Desktop/fileTest" error:&error];
+        NSLog(@"remove = %d,error = %@",isRemove,[error localizedDescription]);
+         */
+        
+        
+        
+        /*
+        // copy file
+        NSError* error = nil;
+        BOOL isMove = [fileM moveItemAtPath:@"/Users/sinosoft/Desktop/fileTest" toPath:@"/Users/sinosoft/Downloads/fileTest" error:&error];
+        NSLog(@"move = %d,error = %@",isMove,[error localizedDescription]);
+        */
+        
+        
         
     }
     return 0;
